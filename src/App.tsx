f9ea94de368bee77be217f0bd8ab15e72133dc14
eskalona17 <fer.escalonso@gmail.com>
2026-05-28 14:04:47 +0200

    install config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
+import "./globals.css";
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-5xl font-bold underline">
         Hello world!
       </h1>
     </>
